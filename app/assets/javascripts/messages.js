f9ad3d274d2410b0be044357856ee9123913c737
Steve Ono <steveono@gmail.com>
2018-05-07 14:57:42 -0600

    Make icons selectable.

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -8,6 +8,14 @@ $(document).on("turbolinks:load", function() {
   $(document).on("click",".expand",function() {
     $(this).parent().next().slideToggle();
   });
+
+  $(document).on("click",".starred",function() {
+    $(this).toggleClass("starred-selected");
+  });
+
+  $(document).on("click",".important-icon",function() {
+    $(this).toggleClass("important-icon-selected");
+  });
 });
 
 
